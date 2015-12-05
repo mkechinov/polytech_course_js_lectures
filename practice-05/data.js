67f6b58ce8af264cd6c93c1e73aa87d380d022a9
Alexander Kurganov <me@akurganow.ru>
2015-12-05 10:03:50 +0300

    Add start scripts

--- practice-05/data.js
+++ practice-05/data.js
@@ -1,20 +1,22 @@
-var request = new XMLHttpRequest();
-var people = [];
-request.open('GET', 'http://cyberman.akurganow.ru/course', true);
+function loadPeoples(callback){
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://cyberman.akurganow.ru/course', true);
 
-request.onload = function() {
-  if (this.status >= 200 && this.status < 400) {
-    // Success!
-    people = JSON.parse(this.response);
-  } else {
-    // We reached our target server, but it returned an error
-    console.error('Server error')
-  }
-};
+	request.onload = function() {
+	  if (this.status >= 200 && this.status < 400) {
+	    // Success!
+	    var people = JSON.parse(this.response);
+	    callback(people);
+	  } else {
+	    // We reached our target server, but it returned an error
+	    console.error('Server error')
+	  }
+	};
 
-request.onerror = function() {
-  // There was a connection error of some sort
-  console.error('Connection error')
-};
+	request.onerror = function() {
+	  // There was a connection error of some sort
+	  console.error('Connection error')
+	};
 
-request.send();
+	request.send();
+}
